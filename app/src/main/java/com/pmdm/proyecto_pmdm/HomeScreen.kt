f0d12700c52b64686dca9fc9c79ec7beba3a122c
Pedro <ppineiroordax@danielcastelao.org>
2024-12-11 23:24:23 +0100

    HomeScreen

---
+++ app/src/main/java/com/pmdm/proyecto_pmdm/HomeScreen.kt
@@ -0,0 +1,38 @@
+package com.pmdm.proyecto_pmdm
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Creador de Archivos") })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { navController.navigate("createFile") }) {
+                Icon(Icons.Default.Add, contentDescription = "Crear Archivo")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Pulsa el botón para crear un archivo")
+        }
+    }
+}
